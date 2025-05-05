dab89194a048ebf53712a7344f3afd724c0a2e7d
Ang3l1llo <angelsanchezmendoz@gmail.com>
2025-05-05 14:24:17 +0200

    Añadido menu principal,pausa y mejoras visuales

--- Scripts/UI_Scripts/menu_pause.gd
+++ Scripts/UI_Scripts/menu_pause.gd
@@ -1,11 +1,29 @@
 extends Control
 
+# Variable para saber si el juego está pausado
+var is_paused = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _ready():
+	# El menú de pausa empieza oculto
+	visible = false
 
+func _input(event: InputEvent):
+	if event.is_action_pressed("pause"):
+		print("pausado")
+		
+		# Ahora se cambia el estado y estaría pausado
+		is_paused = !is_paused
+		get_tree().paused = is_paused
+		
+		# Se hace visible el menú
+		visible = is_paused
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+# Para reiniciar
+func _on_bt_restart_pressed():
+	get_tree().paused = false
+	get_tree().reload_current_scene()
+
+# Para salir al menú principal
+func _on_bt_exit_pressed():
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://Scenes/UI/menu_principal.tscn")
--- Scripts/UI_Scripts/menu_principal.gd
+++ Scripts/UI_Scripts/menu_principal.gd
@@ -1,11 +1,11 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _on_bt_start_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Levels/MeadowLands.tscn")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _on_bt_settings_pressed():
 	pass
+
+func _on_bt_quit_pressed():
+	get_tree().quit()
